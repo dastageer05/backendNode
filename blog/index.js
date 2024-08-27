a78169f03e18d1ebdfe2dc008bf60fbea4a06081
Dastageer <pinjaridastageer@gmail.com>
2024-08-27 20:17:08 +0530

    blog complete

--- blog/index.js
+++ blog/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser")
+const Blog = require('./models/blog')
+const userRoute = require('./routes/user');
+const blogRoute = require('./routes/blog')
 
-const userRoute = require('./routes/user')
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
 const app = express();
 const PORT = 4000;
@@ -12,12 +16,20 @@ mongoose.connect('mongodb://localhost:27017/blogify').then((e) => console.log("M
 app.set('view engine', 'ejs')
 app.set('views', path.resolve('./views'))
 
-app.use(express.urlencoded({ extended: false}))
+app.use(express.urlencoded({ extended: false}));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve('./public')));
 
-app.get('/', (req, res)=>{
-    res.render("home");
+app.get('/', async(req, res)=>{
+    const allBlogs = await Blog.find({});
+    res.render("home", {
+        user: req.user,
+        blogs: allBlogs,
+    });
 });
 
-app.use('/user', userRoute)
+app.use('/user', userRoute);
+app.use('/blog', blogRoute);
 
 app.listen(PORT, ()=> console.log(`Server started at PORT: ${PORT}`));
